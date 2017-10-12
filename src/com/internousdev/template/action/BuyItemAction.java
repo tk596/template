f21aeb282afd05d14d70913b2fe0fc795aefbe14
takuyanakajima <tk_twitter@yahoo.co.jp>
2017-10-12 16:53:37 +0900

    cssの修正

--- src/com/internousdev/template/action/BuyItemAction.java
+++ src/com/internousdev/template/action/BuyItemAction.java
@@ -11,75 +11,75 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class BuyItemAction extends ActionSupport implements SessionAware {
 
-	/**
-	 * アイテム情報を取得
-	 */
-	public BuyItemDAO buyItemDAO = new BuyItemDAO();
+    /**
+    * アイテム情報を取得
+    */
+    public BuyItemDAO buyItemDAO = new BuyItemDAO();
 
-	/**
-	 * アイテム購入個数
-	 */
-	public int count;
+    /**
+    * アイテム購入個数
+    */
+    public int count;
 
-	/**
-	 * 支払い方法
-	 */
-	public String pay;
+    /**
+    * 支払い方法
+    */
+    public String pay;
 
-	/**
-	 * アイテム情報を格納
-	 */
-	public Map<String, Object>  buyItemInfoMap = new HashMap<>();
+    /**
+    * アイテム情報を格納
+    */
+    public Map<String, Object>  buyItemInfoMap = new HashMap<>();
 
-	/**
-	 * 処理結果
-	 */
-	public String result;
+    /**
+    * 処理結果
+    */
+    public String result;
 
-	/**
-	 * 商品情報取得メソッド
-	 *
-	 * @author internous
-	 */
-	public String execute() {
-		result = SUCCESS;
-		buyItemInfoMap.put("count", count);
-		int intCount = Integer.parseInt(buyItemInfoMap.get("count").toString());
-		int intPrice = Integer.parseInt(buyItemInfoMap.get("buyItem_price").toString());
+    /**
+    * 商品情報取得メソッド
+    *
+    * @author internous
+    */
+    public String execute() {
+        result = SUCCESS;
+        buyItemInfoMap.put("count", count);
+        int intCount = Integer.parseInt(buyItemInfoMap.get("count").toString());
+        int intPrice = Integer.parseInt(buyItemInfoMap.get("buyItem_price").toString());
 
-		buyItemInfoMap.put("total_price", intCount * intPrice);
-		String payment;
+        buyItemInfoMap.put("total_price", intCount * intPrice);
+        String payment;
 
-		if(pay.equals("1")) {
+        if(pay.equals("1")) {
 
-			payment = "現金払い";
-			buyItemInfoMap.put("pay", payment);
-		} else {
+            payment = "現金払い";
+            buyItemInfoMap.put("pay", payment);
+        } else {
 
-			payment = "クレジットカード";
-			buyItemInfoMap.put("pay", payment);
-		}
-		return result;
-	}
+            payment = "クレジットカード";
+            buyItemInfoMap.put("pay", payment);
+        }
+        return result;
+    }
 
-	public int getCount() {
-		return count;
-	}
+    public int getCount() {
+        return count;
+    }
 
-	public void setCount(int count) {
-		this.count = count;
-	}
+    public void setCount(int count) {
+        this.count = count;
+    }
 
-	public String getPay() {
-		return pay;
-	}
+    public String getPay() {
+        return pay;
+    }
 
-	public void setPay(String pay) {
-		this.pay = pay;
-	}
+    public void setPay(String pay) {
+        this.pay = pay;
+    }
 
-	@Override
-	public void setSession(Map<String, Object> buyItemInfoMap) {
-		this.buyItemInfoMap = buyItemInfoMap;
-	}
+    @Override
+    public void setSession(Map<String, Object> buyItemInfoMap) {
+        this.buyItemInfoMap = buyItemInfoMap;
+    }
 }
